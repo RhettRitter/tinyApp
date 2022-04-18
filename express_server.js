faa6e9fbdab41f9a20e2d1e3b64cfb943f33cac2
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-18 16:21:21 +0000

    update returning urlDatabse object

--- express_server.js
+++ express_server.js
@@ -4,14 +4,16 @@ const PORT = 8080;
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+    "9sm5xK": "http://www.google.com"
 };
 
 app.get('/', (req, res) => {
     res.send('hello');
-    });
+});
+app.get('/urls.json', (req,res) => {
+    res.json(urlDatabase);
+});
 
-    app.listen(PORT, () => {
-        console.log(`Example app listening on port ${PORT}`);
-    });
-    
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+});
