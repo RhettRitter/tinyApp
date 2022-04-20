45ff06bd2a2cdf1cd3507ca53b7e41c8cdec65c8
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-20 21:42:31 +0000

    added register and login page functionality

--- express_server.js
+++ express_server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+//requires
+const cookieParser = require('cookie-parser')
+app.use(cookieParser());
 
+
+const morgan = require('morgan');
+const { get, redirect } = require('express/lib/response');
+const morganMiddleware = morgan('dev')
+app.use(morganMiddleware)
+app.use(express.urlencoded({ extended: false }))
+
+//helper functions 
 function generateRandomString(numOfChars) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
@@ -11,36 +22,48 @@ function generateRandomString(numOfChars) {
     return result;
 }
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-const morgan = require('morgan')
-const morganMiddleware = morgan('dev')
-app.use(morganMiddleware)
 
-
+//set view engine
 app.set('view engine', 'ejs');
-
+// global objects
 let urlDatabase = {
+
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
 
+let users = {
+    "userRandomID": {
+        id: "userRandomID",
+        email: "user@example.com",
+        password: "boom-boom"
+    },
+    "user2RandomID": {
+        id: "user2RandomID",
+        email: "user2@example.com",
+        password: "boomTown"
+    }
+}
+
+//Gets//
+
+//redirect to login
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.redirect('/login');
+});
+//login Page
+app.get('/login', (req, res) => {
+    res.render('urls_login');
 });
 
-app.get('/urls.json', (req, res) => {
-    res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
+    const templateVars = {
+        username: req.cookies["user"],
+        urls: urlDatabase
+    };
     res.render("urls_index", templateVars);
 });
 
@@ -54,16 +77,89 @@ app.post('/urls', (req, res) => {
     const shortURL = generateRandomString(6);
     urlDatabase[shortURL] = req.body.longURL
     res.redirect(`/urls/${shortURL}`);
+
 });
+//addregistration page
+app.get('/register', (req, res) => {
+    templateVars = {
+        user: req.cookies['userID']
+    };
+    res.render("urls_register", templateVars);
+});
+//add user object to global users object
+app.post('/register', (req, res) => {
+    const id = generateRandomString(4);
+
+    const email = req.body.email;
+
+    const password = req.body.password
+
+    const user = {
+        id: id,
+        email: email,
+        password: password
+
+    };
+
+    users[id] = user;
+    console.log('users', users);
+
+    res.redirect('/login')
+});
+
+
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (!email || !password) {
+        return res.status(401).send('Not registered, please follow registration link');
+    }
+
+
+    let foundUser = null
+    for (const userID in users) {
+        const user = users[userID];
+        //console.log('user:', user);
+        if (user.email === email) {
+            foundUser = user
+            //console.log('userFound');
+            break;
+        }
+    }
+    if (!foundUser) {
+        return res.status(403).send('no email on file')
+    }
+    if (foundUser.password !== password) {
+        return res.status(401).send('wrong password')
+
+
+    }
+    res.cookie('userID', foundUser.ID)
+    res.redirect('/urls')
+
+})
+
+
+
+
 app.post('/urls/:shortURL', (req, res) => {
     const shortURL = req.params.shortURL;
-      urlDatabase[shortURL].longURL = req.body.updatedURL;
-      res.redirect(`/urls`);
+    urlDatabase[shortURL].longURL = req.body.updatedURL;
+    res.redirect(`/urls`);
 
 });
 app.get('/urls/:shortURL', (req, res) => {
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    const templateVars = {
+        username: req.cookies["user"],
+        shortURL: req.params.shortURL,
+        longURL: urlDatabase[req.params.shortURL]
+    };
     res.render("urls_show", templateVars);
+});
+//add logout
+app.post("/logout", (req, res) => {
+    res.clearCookie('UserId');
+    res.redirect('/login')
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
